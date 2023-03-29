0339be489678a53bcb7f3b81d6bc82a9952002d6
Behichev <99028430+Behichev@users.noreply.github.com>
2023-03-29 21:33:29 +0300

    Initial Commit

---
+++ Cleaner Test App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cleaner Test App
+//
+//  Created by Ivan Behichev on 29.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
